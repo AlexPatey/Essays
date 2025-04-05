044a7880fd330bf2ea1e43f2fcadbabe4fa82c6e
Alex Patey <alex.patey@btinternet.com>
2025-04-05 18:01:31 +0100

    wip adding routing etc.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {GenesisComponent} from './genesis/genesis.component';
+import {HomeComponent} from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'genesis', component: GenesisComponent},
+];
---
+++ src/app/genesis/genesis.component.ts
@@ -0,0 +1,8 @@
+import { Component} from '@angular/core';
+@Component({
+  selector: 'app-genesis',
+  standalone: true,
+  templateUrl: './genesis.component.html',
+  styleUrl: './genesis.component.css',
+})
+export class GenesisComponent {}
